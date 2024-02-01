84bce1519f605497304c220b8d8de83db08ee2e2
rajkarthik <121234659+rajkarthik2003@users.noreply.github.com>
2024-02-01 11:10:01 +0530

    java List

---
+++ List/ArrayListone.java
@@ -0,0 +1,33 @@
+package List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListone {
+    public static void main(String[] args) {
+        // Creating an ArrayList
+        List<String> fruits = new ArrayList<>();
+
+        // Adding elements
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Orange");
+
+        // Accessing elements
+        System.out.println("Fruits: " + fruits);
+        //System.out.println("added fruits");
+        // Iterating through the list
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+
+        // Removing an element
+        fruits.remove("Banana");
+
+        // Checking if an element exists
+        System.out.println("Contains 'Banana': " + fruits.contains("Banana"));
+
+        // Size of the list
+        System.out.println("Size: " + fruits.size());
+    }
+}
