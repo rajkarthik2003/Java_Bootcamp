f9924a95d71766685873ff759ed6d04afafdd3cb
rajkarthik <121234659+rajkarthik2003@users.noreply.github.com>
2024-02-01 16:18:18 +0530

    Update Person.java

--- oop/copy_constructor/Person.java
+++ oop/copy_constructor/Person.java
@@ -13,14 +13,6 @@ public class Person {
     String[] passport;
     int seatNumber;
 
-    /**
-     * Constructs a new Person object with the given attributes.
-     *
-     * @param name        The name of the person
-     * @param nationality The nationality of the person
-     * @param dateOfBirth The date of birth of the person
-     * @param seatNumber  The seat number of the person
-     */
     public Person(String name, String nationality, String dateOfBirth, int seatNumber) {
         // set fields equal to parameters here.
         this.name = name;
@@ -31,14 +23,6 @@ public class Person {
 
     }
 
-    /**
-     * Creates a new Person object by copying the attributes from another Person
-     * object.
-     *
-     * @param src The Person object to copy from
-     */
-    // copy constructor
-
     public Person(Person src) {
         this.name = src.name;
         this.nationality = src.nationality;
@@ -48,26 +32,10 @@ public class Person {
     }
 
     public static void main(String[] args) {
-        /**
-         * Creates a new Person object with the specified attributes.
-         *
-         * @param name        The name of the person.
-         * @param nationality The nationality of the person.
-         * @param dateOfBirth The date of birth of the person in the format
-         *                    "MM/DD/YYYY".
-         * @param height      The height of the person in feet.
-         * @return A new Person object with the specified attributes.
-         */
+      
         Person person = new Person("Rayan Slim", "Canadian", "01/01/1111", 5);
 
-        /**
-         * Creates a new Person object that is a twin of the given person.
-         * The twin has the same attributes as the original person, except for the name
-         * and seat number.
-         *
-         * @param person The original person object
-         * @return The twin person object with modified name and seat number
-         */
+        
         Person twin = new Person(person);
         twin.name = "Jad Slim";
         twin.seatNumber = 4;
