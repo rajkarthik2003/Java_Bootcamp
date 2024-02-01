b1d63f71ba3d4c86c265506b802b44218ecd6306
rajkarthik <121234659+rajkarthik2003@users.noreply.github.com>
2024-02-01 16:29:37 +0530

    spring crud

---
+++ java1/src/main/java/com/example/java1/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.example.java1.repository;
+
+
+import com.example.java1.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, String> {
+}
