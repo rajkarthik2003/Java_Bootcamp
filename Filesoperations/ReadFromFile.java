54b019a6410a29e50590067e2bddb854c5dfcdce
rajkarthik <121234659+rajkarthik2003@users.noreply.github.com>
2024-02-01 16:12:51 +0530

    Update ReadFromFile.java

--- Filesoperations/ReadFromFile.java
+++ Filesoperations/ReadFromFile.java
@@ -8,7 +8,20 @@ import java.util.List;
 public class ReadFromFile {
     public static void main(String[] args) {
        
-        Path filePath = Paths.get("C:\\Users\\Simon\\Desktop\\newfile.txt");
+       package Filesoperations;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileExits {
+    public static void main(String[] args) {
+        
+         Path filePath = Paths.get("C:\\Users\\karthik\\Desktop\\newfile.txt");
+
+        System.out.println("File exists: " + Files.exists(filePath);
+    }
+}
 
         try {
             // Read all lines from the file into a List
