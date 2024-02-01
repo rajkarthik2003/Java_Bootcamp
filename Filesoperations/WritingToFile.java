6676289ff776f1c22549c5f9a377e95170817fa0
rajkarthik <121234659+rajkarthik2003@users.noreply.github.com>
2024-02-01 16:13:17 +0530

    Update WritingToFile.java

--- Filesoperations/WritingToFile.java
+++ Filesoperations/WritingToFile.java
@@ -8,7 +8,20 @@ import java.util.List;
 public class WritingToFile {
     public static void main(String[] args) {
         // Specify the path to the file
-        Path filePath = Paths.get("C:\\Users\\Simon\\Desktop\\newfile.txt");
+        package Filesoperations;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileExits {
+    public static void main(String[] args) {
+        
+         Path filePath = Paths.get("C:\\Users\\karthik\\Desktop\\newfile.txt");
+
+        System.out.println("File exists: " + Files.exists(filePath);
+    }
+}
 
         // Data to be written to the newfile
         List<String> content = Arrays.asList("Line 1", "Line 2", "Line 3");
